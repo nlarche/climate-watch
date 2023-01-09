84bdb84d56a9527ab79b2371dee2d126e45afd7f
Nicolas LARCHE <nlarche@zeenea.com>
2023-01-09 17:15:57 +0100

    Add react ui

---
+++ src/ui/react/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { SearchInput } from "./components/searchInput";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Climate Watch</h2>
+      <SearchInput></SearchInput>
+    </div>
+  );
+}
+
+export default App;
